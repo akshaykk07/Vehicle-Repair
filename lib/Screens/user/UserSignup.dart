ccdc2df67a64bfbb9924ffeb9bc82f01e7103707
akshaykk07 <akshaykuruvangad@gmail.com>
2023-12-28 00:04:51 +0530

    first

--- lib/Screens/user/UserSignup.dart
+++ lib/Screens/user/UserSignup.dart
@@ -5,14 +5,20 @@ import 'package:flutter_application_1/widgets/customTextfield.dart';
 import 'package:flutter_application_1/widgets/apptext.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class UserSignup extends StatelessWidget {
+class UserSignup extends StatefulWidget {
   UserSignup({super.key});
 
   @override
+  State<UserSignup> createState() => _UserSignupState();
+}
+
+class _UserSignupState extends State<UserSignup> {
+  @override
   final username = TextEditingController();
   final phone = TextEditingController();
-  final email = TextEditingController();
+  var email = TextEditingController();
   final password = TextEditingController();
+  final formkey = GlobalKey<FormState>();
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,92 +36,118 @@ class UserSignup extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.only(left: 45, right: 45).r,
         child: SingleChildScrollView(
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 60).r,
-              child: Image.asset(
-                "assets/logo.png",
-                width: 140.w,
-                height: 140.h,
-              ),
-            ),
-            SizedBox(
-              height: 50.h,
-            ),
-            const AppText(
-                text: "SIGN UP",
-                weight: FontWeight.w700,
-                size: 23,
-                textcolor: customBalck),
-            SizedBox(
-              height: 50.h,
-            ),
-            const Align(
-              alignment: Alignment.bottomLeft,
-              child: AppText(
-                  text: "Enter Username",
-                  weight: FontWeight.w500,
-                  size: 16,
-                  textcolor: customBalck),
-            ),
-            CustomTextField( //Custom Textfield..............
-                hint: "Enter Username",
-                controller: username,
-                validator: (value) {}),
-
-            const Align(
-              alignment: Alignment.bottomLeft,
-              child: AppText(
-                  text: "Enter Phone number",
-                  weight: FontWeight.w500,
-                  size: 16,
-                  textcolor: customBalck),
-            ),
-            CustomTextField(
-                hint: "Enter Phone number",
-                controller: phone,
-                kebordtype: TextInputType.number,
-                validator: (value) {}),
-
-            const Align(
-              alignment: Alignment.bottomLeft,
-              child: AppText(
-                  text: "Enter your email",
-                  weight: FontWeight.w500,
-                  size: 16,
-                  textcolor: customBalck),
-            ),
-            CustomTextField(
-                hint: "Enter your email",
-                controller: email,
-                kebordtype: TextInputType.emailAddress,
-                validator: (value) {}),
-
-            const Align(
-              alignment: Alignment.bottomLeft,
-              child: AppText(
-                  text: "Enter your password",
-                  weight: FontWeight.w500,
-                  size: 16,
-                  textcolor: customBalck),
-            ),
-            CustomTextField(
-                hint: "Enter your password",
-                controller: password,
-                validator: (value) {}),
-            SizedBox(
-              height: 30.h,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 50, right: 50, bottom: 30).r,
-              child: CustomButton( //sign up button.......
-                  btnname: "SIGN UP",
-                  btntheam: customBlue,
-                  textcolor: white,
-                  click: () {}),
-            ),
-          ]),
+          child: Form(
+            key: formkey,
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 70).r,
+                    child: Image.asset(
+                      "assets/logo.png",
+                      width: 140.w,
+                      height: 140.h,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40.h,
+                  ),
+                  const AppText(
+                      text: "SIGN UP",
+                      weight: FontWeight.w700,
+                      size: 23,
+                      textcolor: customBalck),
+                  SizedBox(
+                    height: 50.h,
+                  ),
+                  const Align(
+                    alignment: Alignment.bottomLeft,
+                    child: AppText(
+                        text: "Enter Username",
+                        weight: FontWeight.w500,
+                        size: 16,
+                        textcolor: customBalck),
+                  ),
+                  CustomTextField(
+                      //Custom Textfield..............
+                      hint: "Enter Username",
+                      controller: username,
+                      validator: (value) {
+                        if (value!.isEmpty || value == null) {
+                          return "enter username";  // validation............
+                        }
+                      }),
+                  const Align(
+                    alignment: Alignment.bottomLeft,
+                    child: AppText(
+                        text: "Enter Phone number",
+                        weight: FontWeight.w500,
+                        size: 16,
+                        textcolor: customBalck),
+                  ),
+                  CustomTextField(
+                      hint: "Enter Phone number",
+                      controller: phone,
+                      kebordtype: TextInputType.number,
+                      validator: (value) {
+                        if (value?.length != 10) {      // validation............
+                          return 'Please enter mobile number';
+                        }
+                      }),
+                  const Align(
+                    alignment: Alignment.bottomLeft,
+                    child: AppText(
+                        text: "Enter your email",
+                        weight: FontWeight.w500,
+                        size: 16,
+                        textcolor: customBalck),
+                  ),
+                  CustomTextField(
+                      hint: "Enter your email",
+                      controller: email,
+                      kebordtype: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value!.isEmpty ||
+                            !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                .hasMatch(value)) {  // validation............
+                          return 'Enter a valid email!';
+                        }
+                      }),
+                  const Align(
+                    alignment: Alignment.bottomLeft,
+                    child: AppText(
+                        text: "Enter your password",
+                        weight: FontWeight.w500,
+                        size: 16,
+                        textcolor: customBalck),
+                  ),
+                  CustomTextField(
+                      hint: "Enter your password",
+                      obscure: true,
+                      controller: password,
+                      validator: (value) {
+                        if (value!.isEmpty || value == null) {
+                          return "enter password";   // validation............
+                        }
+                      }),
+                  SizedBox(
+                    height: 30.h,
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 50, right: 50, bottom: 30)
+                            .r,
+                    child: CustomButton(
+                        //sign up button.......
+                        btnname: "SIGN UP",
+                        btntheam: customBlue,
+                        textcolor: white,
+                        click: () {
+                          formkey.currentState!.validate();
+                        }),
+                  ),
+                ]),
+          ),
         ),
       ),
     );
