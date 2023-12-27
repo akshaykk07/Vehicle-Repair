b5924da6ba3efe9aea80727f27502399a78d870f
akshaykk07 <akshaykuruvangad@gmail.com>
2023-12-28 00:11:08 +0530

    first

--- lib/Screens/user/UserLogin.dart
+++ lib/Screens/user/UserLogin.dart
@@ -13,8 +13,8 @@ class UserLogin extends StatelessWidget {
 
   @override
   final username = TextEditingController();
-
   final password = TextEditingController();
+  final formkey = GlobalKey<FormState>();
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,113 +23,128 @@ class UserLogin extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.only(left: 45, right: 45, top: 100).r,
         child: SingleChildScrollView(
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-            Image.asset(
-              "assets/logo.png",
-              width: 140.w,
-              height: 140.h,
-            ),
-            SizedBox(
-              height: 50.h,
-            ),
-            const AppText(
-                text: "LOGIN",
-                weight: FontWeight.w700,
-                size: 23,
-                textcolor: customBalck),
-            SizedBox(
-              height: 50.h,
-            ),
-            const Align(
-              alignment: Alignment.bottomLeft,
-              child: AppText(
-                  text: "Enter Username",
-                  weight: FontWeight.w500,
-                  size: 16,
-                  textcolor: customBalck),
-            ),
-            CustomTextField(
-                hint: "username", controller: username, validator: (value) {}),
-            SizedBox(
-              height: 10.h,
-            ),
-            const Align(
-              alignment: Alignment.bottomLeft,
-              child: AppText(
-                  text: "Enter Password",
-                  weight: FontWeight.w500,
-                  size: 16,
-                  textcolor: customBalck),
-            ),
-            CustomTextField(
-                hint: "password",
-                controller: password,
-                obscure: true,
-                validator: (value) {}),
-            SizedBox(
-              height: 20.h,
-            ),
-            Align(
-                alignment: Alignment.centerRight,
-                child: InkWell(
-                  onTap: () {
-                    // Forgot password......................
-                  },
-                  child: const AppText(
-                      text: "Forgot password ?",
-                      weight: FontWeight.w400,
-                      size: 14,
+          child: Form(
+            key: formkey,
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/logo.png",
+                    width: 140.w,
+                    height: 140.h,
+                  ),
+                  SizedBox(
+                    height: 50.h,
+                  ),
+                  const AppText(
+                      text: "LOGIN",
+                      weight: FontWeight.w700,
+                      size: 23,
                       textcolor: customBalck),
-                )),
-            SizedBox(
-              height: 60.h,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 50, right: 50).r,
-              child: CustomButton(
-                  btnname: "LOGIN", // Login Button.....
-                  btntheam: customBlue,
-                  textcolor: white,
-                  click: () {
-                    Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => UserHome(),
-                        ));
-                  }),
-            ),
-            SizedBox(
-              height: 20.h,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const AppText(
-                    text: "Do you have account ?",
-                    weight: FontWeight.w400,
-                    size: 13,
-                    textcolor: customBalck),
-                SizedBox(
-                  width: 10.w,
-                ),
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => UserSignup(),
-                        )); // SignUp ..................................
-                  },
-                  child: const AppText(
-                      text: "Sign up",
-                      weight: FontWeight.w400,
-                      size: 13,
-                      textcolor: customBlue),
-                )
-              ],
-            ),
-          ]),
+                  SizedBox(
+                    height: 50.h,
+                  ),
+                  const Align(
+                    alignment: Alignment.bottomLeft,
+                    child: AppText(
+                        text: "Enter Username",
+                        weight: FontWeight.w500,
+                        size: 16,
+                        textcolor: customBalck),
+                  ),
+                  CustomTextField(
+                      hint: "username",
+                      controller: username,
+                      validator: (value) {
+                        if (value!.isEmpty || value == null) {
+                          return "enter username";// validation.................
+                        }
+                      }),
+                  SizedBox(
+                    height: 10.h,
+                  ),
+                  const Align(
+                    alignment: Alignment.bottomLeft,
+                    child: AppText(
+                        text: "Enter Password",
+                        weight: FontWeight.w500,
+                        size: 16,
+                        textcolor: customBalck),
+                  ),
+                  CustomTextField(
+                      hint: "password",
+                      controller: password,
+                      obscure: true,
+                      validator: (value) {
+                        if (value!.isEmpty || value == null) { // validation.................
+                          return "enter password";
+                        }
+                      }),
+                  SizedBox(
+                    height: 20.h,
+                  ),
+                  Align(
+                      alignment: Alignment.centerRight,
+                      child: InkWell(
+                        onTap: () {
+                          // Forgot password......................
+                        },
+                        child: const AppText(
+                            text: "Forgot password ?",
+                            weight: FontWeight.w400,
+                            size: 14,
+                            textcolor: customBalck),
+                      )),
+                  SizedBox(
+                    height: 60.h,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 50, right: 50).r,
+                    child: CustomButton(
+                        btnname: "LOGIN", // Login Button.....
+                        btntheam: customBlue,
+                        textcolor: white,
+                        click: () {
+                          //formkey.currentState!.validate();
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => UserHome(),
+                              ));
+                        }),
+                  ),
+                  SizedBox(
+                    height: 20.h,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const AppText(
+                          text: "Do you have account ?",
+                          weight: FontWeight.w400,
+                          size: 13,
+                          textcolor: customBalck),
+                      SizedBox(
+                        width: 10.w,
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => UserSignup(),
+                              )); // SignUp ..................................
+                        },
+                        child: const AppText(
+                            text: "Sign up",
+                            weight: FontWeight.w400,
+                            size: 13,
+                            textcolor: customBlue),
+                      )
+                    ],
+                  ),
+                ]),
+          ),
         ),
       ),
     );
